151579a379345585d0b7adb4f31896b7d859c381
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-23 20:31:26 +0800

    [guessNumber] [Alisa] feature:return 4A0B when guessNumber of correct position and number

--- src/main/java/guessNumber/GuessNumber.java
+++ src/main/java/guessNumber/GuessNumber.java
@@ -1,7 +1,14 @@
 package guessNumber;
 
+import java.util.Arrays;
+
 public class GuessNumber {
+    int[] answer = {1, 2, 3, 4};
+
     public String guessNumber(int[] inputNumber) {
+        if (Arrays.equals(inputNumber, answer)) {
+            return "4A0B";
+        }
         return null;
     }
 }
