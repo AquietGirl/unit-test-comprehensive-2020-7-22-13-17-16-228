28aa8327ffffd014162f8f058e6f1b70f8e2e61b
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-23 22:36:08 +0800

    [guessNumber] [Alisa] refactor: organize combined code

--- src/main/java/guessNumber/GuessNumber.java
+++ src/main/java/guessNumber/GuessNumber.java
@@ -7,47 +7,17 @@ public class GuessNumber {
     int[] answer = {1, 2, 3, 4};
 
     public String guessNumber(int[] inputNumber) {
-        boolean isHasEqual = false;
-        if (Arrays.equals(inputNumber, answer)) {
-            return "4A0B";
-        }
-        for (int i = 0; i < inputNumber.length; i++) {
-            if (inputNumber[i] == answer[i]) {
-                isHasEqual = true;
-            }
-        }
-        int b = 0;
-        for (int i = 0; i < inputNumber.length; i++) {
-            int index = i;
-            if (IntStream.of(answer).anyMatch(x -> x == inputNumber[index])) {
-                b++;
-            }
-        }
-        if (!isHasEqual && b == 4) {
-            return "0A4B";
-        }
-
-        int a = 0;
-        for (int i = 0; i < inputNumber.length; i++) {
-            if (inputNumber[i] == answer[i]) {
+        int a = 0, b = 0;
+        for (int index = 0; index < inputNumber.length; index++) {
+            if (inputNumber[index] == answer[index]) {
                 a++;
+            } else {
+                int finalIndex = index;
+                if (IntStream.of(answer).anyMatch(x -> x == inputNumber[finalIndex])) {
+                    b++;
+                }
             }
         }
-        if (a == 1 && b-a ==3) {
-            return "1A3B";
-        }
-
-        if (a == 1 && b-a ==1) {
-            return "1A1B";
-        }
-
-        if (!isHasEqual && b == 2){
-            return "0A2B";
-        }
-
-        if (!isHasEqual) {
-            return "0A0B";
-        }
-        return null;
+        return a + "A" + b + "B";
     }
 }
