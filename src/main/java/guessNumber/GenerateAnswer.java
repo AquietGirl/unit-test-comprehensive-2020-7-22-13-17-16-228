225cece59d0cdb7bda13a1c4f808e213db66603b
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-23 23:06:23 +0800

    [guessNumber] [Alisa] feature: should_return_answer_with_4_length_when_generate_answer

--- src/main/java/guessNumber/GenerateAnswer.java
+++ src/main/java/guessNumber/GenerateAnswer.java
@@ -2,6 +2,26 @@ package guessNumber;
 
 public class GenerateAnswer {
     public int[] generate() {
-        return null;
+        int length = 4;
+        int maxNum = 9;
+        int minNum = 0;
+        int[] result = new int[length];
+        int count = 0;
+        while (count < length) {
+            int num = (int) (Math.random() * (maxNum - minNum)) + minNum;
+            boolean flag = true;
+            for (int j = 0; j < length; j++) {
+                if (num == result[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                result[count] = num;
+                count++;
+            }
+        }
+        return result;
     }
+
 }
