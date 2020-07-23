60b99bb6c7cfee3d5bd979bb41976ca6b320e641
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-23 22:54:14 +0800

    [guessNumber] [Alisa] feature: should_return_wrong_input_input_again_when_has_repeat_number

--- src/main/java/guessNumber/GuessNumber.java
+++ src/main/java/guessNumber/GuessNumber.java
@@ -1,15 +1,22 @@
 package guessNumber;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.stream.IntStream;
 
 public class GuessNumber {
     int[] answer = {1, 2, 3, 4};
 
     public String guessNumber(int[] inputNumber) {
+
         if (inputNumber.length != 4) {
             return "Wrong Input,Input again";
         }
+
+        if (!isNotRepeat(inputNumber)) {
+            return "Wrong Input,Input again";
+        }
+
         int a = 0, b = 0;
         for (int index = 0; index < inputNumber.length; index++) {
             if (inputNumber[index] == answer[index]) {
@@ -22,5 +29,13 @@ public class GuessNumber {
             }
         }
         return a + "A" + b + "B";
+    }
+
+    public boolean isNotRepeat(int[] array) {
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+        for (int value : array) {
+            hashSet.add(value);
+        }
+        return hashSet.size() == array.length;
     }
 }
