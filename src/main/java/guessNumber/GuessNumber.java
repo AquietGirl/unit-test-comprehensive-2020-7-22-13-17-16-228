1f4fd1c5c9581e8e3d225a238925249bfa5a1b49
huangal5 <alisa.y.s.huang@oocl.com>
2020-07-23 20:45:05 +0800

    [guessNumber] [Alisa] feature:return 0A0B when guessNumber of incorrect position and number

--- src/main/java/guessNumber/GuessNumber.java
+++ src/main/java/guessNumber/GuessNumber.java
@@ -6,8 +6,17 @@ public class GuessNumber {
     int[] answer = {1, 2, 3, 4};
 
     public String guessNumber(int[] inputNumber) {
+        boolean isHasEqual = false;
         if (Arrays.equals(inputNumber, answer)) {
             return "4A0B";
+        }
+        for (int i = 0; i< inputNumber.length;i++){
+            if (inputNumber[i] == answer[i]){
+                isHasEqual = true;
+            }
+        }
+        if (!isHasEqual){
+            return "0A0B";
         }
         return null;
     }
